c3dded50e6032dbecbc9a4401479c296bd51e7fe
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-08 21:39:40 +0200

    creating class Beer

---
+++ src/main/java/com/example/spring_demo/models/Beer.java
@@ -0,0 +1,46 @@
+package com.example.spring_demo.models;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public class Beer {
+    @Positive(message = "id should be positive!")
+    private int id;
+    @NotNull(message = "entity can't be empty")
+    @Size(min = 2, max = 20, message = "name should be between 2 and 20 symbols!")
+    private String name;
+    @Positive(message = "abv should be positive!")
+    private double abv;
+
+
+    public Beer(int id, String name, double abv) {
+        this.id = id;
+        this.name = name;
+        this.abv = abv;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAbv() {
+        return abv;
+    }
+
+    public void setAbv(double abv) {
+        this.abv = abv;
+    }
+}
