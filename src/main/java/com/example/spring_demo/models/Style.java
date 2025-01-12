8bd1bd341e227a09b22e31cef95747c6196993e6
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-12 22:01:43 +0200

    updating Style

--- src/main/java/com/example/spring_demo/models/Style.java
+++ src/main/java/com/example/spring_demo/models/Style.java
@@ -1,9 +1,14 @@
 package com.example.spring_demo.models;
 
+import java.util.Objects;
+
 public class Style {
 
     private int id;
     private String name;
+
+    public Style() {
+    }
 
     public Style(int id, String name) {
         this.id = id;
@@ -24,5 +29,18 @@ public class Style {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Style style = (Style) o;
+        return id == style.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
