a6fc4d59ac33a98225f677914d7920384f7532e5
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-13 23:52:40 +0200

    changes in Beer class

--- src/main/java/com/example/spring_demo/models/Beer.java
+++ src/main/java/com/example/spring_demo/models/Beer.java
@@ -10,15 +10,25 @@ public class Beer {
     private String name;
     private double abv;
     private Style style;
+    private User createdBy;
 
     public Beer() {
     }
 
-    public Beer(int id, String name, double abv, Style style) {
+    public Beer(int id, String name, double abv, Style style, User creator) {
         this.id = id;
         this.name = name;
         this.abv = abv;
         this.style = style;
+        this.createdBy = creator;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
     }
 
     public Style getStyle() {
