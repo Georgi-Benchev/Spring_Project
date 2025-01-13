3193c35667843fba342a92016300dee7b465831a
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-13 23:51:24 +0200

    adding UserRepository interface

---
+++ src/main/java/com/example/spring_demo/repositorys/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.spring_demo.repositorys;
+
+import com.example.spring_demo.models.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    List<User> getAll();
+
+    User getById(int id);
+
+    User getByUsername(String username);
+}
