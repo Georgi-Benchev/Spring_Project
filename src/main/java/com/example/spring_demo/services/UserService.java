1b0fa215f12dc166ace2ec3ee8b33dd161c0f52b
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-13 23:52:11 +0200

    adding UserService interface

---
+++ src/main/java/com/example/spring_demo/services/UserService.java
@@ -0,0 +1,14 @@
+package com.example.spring_demo.services;
+
+import com.example.spring_demo.models.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAll();
+
+    User getById(int id);
+
+    User getByUsername(String username);
+}
