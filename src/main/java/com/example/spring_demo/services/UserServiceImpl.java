52df3f7d60d89f4f8c1a82780f34ef36dd868800
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-13 23:51:56 +0200

    adding UserServiceImpl class

---
+++ src/main/java/com/example/spring_demo/services/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.spring_demo.services;
+
+import com.example.spring_demo.models.User;
+import com.example.spring_demo.repositorys.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.getAll();
+    }
+
+    @Override
+    public User getById(int id) {
+        return userRepository.getById(id);
+    }
+
+    @Override
+    public User getByUsername(String username) {
+        return userRepository.getByUsername(username);
+    }
+}
