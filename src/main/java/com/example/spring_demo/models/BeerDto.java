790f2800fbe29763c74f59bfb3522d900e5bccce
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-15 10:57:58 +0200

    renaming BeerDTO to BeerDto

--- src/main/java/com/example/spring_demo/models/BeerDto.java
+++ src/main/java/com/example/spring_demo/models/BeerDto.java
@@ -4,17 +4,24 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
-public class BeerDTO {
+public class BeerDto {
     @NotNull(message = "Name can't be empty")
     @Size(min = 2, max = 20, message = "Name should be between 2 and 20 symbols!")
     private String name;
+
     @Positive(message = "ABV should be positive!")
     private double abv;
+
     @Positive(message = "StyleId should be positive")
     private int styleId;
 
 
-    public BeerDTO() {
+    public BeerDto() {
+    }
+
+    public BeerDto(String name, double abv) {
+        this.name = name;
+        this.abv = abv;
     }
 
     public String getName() {
