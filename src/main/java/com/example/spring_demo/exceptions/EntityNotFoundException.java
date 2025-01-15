08fca90c6329f5996d014fcc9846c9bc047d49e3
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-15 11:00:46 +0200

    adding new constructor for EntityNotFoundException

--- src/main/java/com/example/spring_demo/exceptions/EntityNotFoundException.java
+++ src/main/java/com/example/spring_demo/exceptions/EntityNotFoundException.java
@@ -4,4 +4,8 @@ public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String type, String attribute, String value) {
         super(String.format("%s with %s %s not found.", type, attribute, value));
     }
+
+    public EntityNotFoundException(String type, int id) {
+        super(String.format("%s with id %d not found.", type, id));
+    }
 }
