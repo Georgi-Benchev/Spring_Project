0f19cda82a695e0aeb3b0a7532163a63d3185440
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-15 11:06:08 +0200

    creating UserController

---
+++ src/main/java/com/example/spring_demo/controllers/UserController.java
@@ -0,0 +1,50 @@
+package com.example.spring_demo.controllers;
+
+import com.example.spring_demo.exceptions.EntityNotFoundException;
+import com.example.spring_demo.models.Beer;
+import com.example.spring_demo.models.User;
+import com.example.spring_demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        try {
+            return userService.getById(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}/wishList")
+    public List<Beer> getWishList(@PathVariable int id) {
+        return new ArrayList<>(getUserById(id).getWishList());
+    }
+}
