35d502a0fb89d1b133618b29401be5fb6f719260
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-15 10:58:51 +0200

    updating BeerMapper

--- src/main/java/com/example/spring_demo/helpers/BeerMapper.java
+++ src/main/java/com/example/spring_demo/helpers/BeerMapper.java
@@ -1,33 +1,38 @@
 package com.example.spring_demo.helpers;
 
 import com.example.spring_demo.models.Beer;
-import com.example.spring_demo.models.BeerDTO;
-import com.example.spring_demo.services.StyleService;
+import com.example.spring_demo.models.BeerDto;
+import com.example.spring_demo.repositorys.BeerRepository;
+import com.example.spring_demo.repositorys.StyleRepository;
+import com.example.spring_demo.repositorys.StyleRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BeerMapper {
 
-    private final StyleService styleService;
+    private final StyleRepository styleRepositoryImpl;
+    private final BeerRepository beerRepository;
 
     @Autowired
-    public BeerMapper(StyleService styleService) {
-        this.styleService = styleService;
+    public BeerMapper(BeerRepository beerRepository, StyleRepositoryImpl styleRepositoryImpl) {
+        this.beerRepository = beerRepository;
+        this.styleRepositoryImpl = styleRepositoryImpl;
     }
 
 
-    public Beer fromDto(int id, BeerDTO dto) {
+    public Beer fromDto(int id, BeerDto dto) {
         Beer beer = fromDto(dto);
         beer.setId(id);
+        beer.setCreatedBy(beerRepository.getBeerById(id).getCreatedBy());
         return beer;
     }
 
-    public Beer fromDto(BeerDTO dto) {
+    public Beer fromDto(BeerDto dto) {
         Beer beer = new Beer();
         beer.setName(dto.getName());
         beer.setAbv(dto.getAbv());
-        beer.setStyle(styleService.getById(dto.getStyleId()));
+        beer.setStyle(styleRepositoryImpl.getById(dto.getStyleId()));
         return beer;
     }
 }
