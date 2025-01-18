996b9d19b2f2e60051f88562e07b49b527db3606
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-18 17:46:39 +0200

    changes in service interface

--- src/main/java/com/example/spring_demo/services/BeerService.java
+++ src/main/java/com/example/spring_demo/services/BeerService.java
@@ -1,15 +1,14 @@
 package com.example.spring_demo.services;
 
 import com.example.spring_demo.models.Beer;
+import com.example.spring_demo.models.FilterOptions;
 import com.example.spring_demo.models.User;
 
 import java.util.List;
 
 public interface BeerService {
-    /*
-        List<Beer> getAll(String name, Double minAbv, Double maxAbv, Integer styleId, String sortBy, String orderBy);
-    */
-    List<Beer> getAll();
+
+    List<Beer> getAll(FilterOptions filterOptions);
 
     Beer getBeerById(int id);
 
