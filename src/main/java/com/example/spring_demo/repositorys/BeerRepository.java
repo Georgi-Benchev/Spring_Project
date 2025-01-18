c946e5a0e42d49980558d303497b2c230e600231
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-18 17:46:54 +0200

    changes in repo interface

--- src/main/java/com/example/spring_demo/repositorys/BeerRepository.java
+++ src/main/java/com/example/spring_demo/repositorys/BeerRepository.java
@@ -1,20 +1,14 @@
 package com.example.spring_demo.repositorys;
 
 import com.example.spring_demo.models.Beer;
+import com.example.spring_demo.models.FilterOptions;
 import com.example.spring_demo.models.User;
 
 import java.util.List;
 
-public interface BeerRepository {
-    /*
-        List<Beer> getAll(String name,
-                       Double minAbv,
-                       Double maxAbv,
-                       Integer styleId,
-                       String sortBy,
-                       String orderBy);
-    */
-    List<Beer> getAll();
+public interface BeerRepository{
+
+    List<Beer> getAll(FilterOptions filterOptions);
 
     Beer getBeerById(int id);
 
