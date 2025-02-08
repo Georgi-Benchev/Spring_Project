d37bdec489d55474d9c7eca49dd864b37fb1cb4c
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-08 20:20:43 +0200

    adding createUser validation in service layer

--- src/main/java/com/example/spring_demo/services/UserServiceImpl.java
+++ src/main/java/com/example/spring_demo/services/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.spring_demo.services;
 
+import com.example.spring_demo.exceptions.DuplicateEntityException;
+import com.example.spring_demo.exceptions.EntityNotFoundException;
 import com.example.spring_demo.models.User;
 import com.example.spring_demo.repositorys.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +32,21 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getByUsername(String username) {
         return userRepository.getByUsername(username);
+    }
+
+    @Override
+    public void createUser(User user) {
+        boolean userExists = true;
+        try {
+            userRepository.getByUsername(user.getUsername());
+        } catch (EntityNotFoundException e) {
+            userExists = false;
+        }
+
+        if (userExists) {
+            throw new DuplicateEntityException("User", "Username", user.getUsername());
+        }
+
+        userRepository.createUser(user);
     }
 }
