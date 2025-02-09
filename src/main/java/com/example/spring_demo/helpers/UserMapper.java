9b604ec2649030d7822e5b73530621a6602706c2
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-09 23:17:04 +0200

    creating user mapper

---
+++ src/main/java/com/example/spring_demo/helpers/UserMapper.java
@@ -0,0 +1,21 @@
+package com.example.spring_demo.helpers;
+
+
+import com.example.spring_demo.models.RegistrationRequest;
+import com.example.spring_demo.models.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User getUserFromDto(RegistrationRequest registrationRequest) {
+        User user = new User();
+        user.setAdmin(false);
+        user.setFirstName(registrationRequest.getFirst_name());
+        user.setLastName(registrationRequest.getLast_name());
+        user.setUsername(registrationRequest.getUsername());
+        user.setPassword(registrationRequest.getPassword());
+        user.setEmail(registrationRequest.getEmail());
+        return user;
+    }
+}
