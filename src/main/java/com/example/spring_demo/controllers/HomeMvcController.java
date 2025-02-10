de7be8ae246643798e63c2a350521df313545f24
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-10 19:38:27 +0200

    adding dynamic homepage with login/logout

--- src/main/java/com/example/spring_demo/controllers/HomeMvcController.java
+++ src/main/java/com/example/spring_demo/controllers/HomeMvcController.java
@@ -1,5 +1,9 @@
 package com.example.spring_demo.controllers;
 
+import com.example.spring_demo.helpers.AuthenticationHelper;
+import com.example.spring_demo.models.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeMvcController {
 
+    private final AuthenticationHelper authenticationHelper;
+
+    @Autowired
+    public HomeMvcController(AuthenticationHelper authenticationHelper) {
+        this.authenticationHelper = authenticationHelper;
+    }
+
     @GetMapping
-    public String getHomepage(Model model) {
+    public String getHomepage(Model model, HttpSession session) {
+        try {
+            User user = authenticationHelper.tryGetUser(session);
+            model.addAttribute("isUserLoggedIn", true);
+            model.addAttribute("user", user);
+        } catch (Exception e) {
+            model.addAttribute("isUserLoggedIn", false);
+        }
         return "HomeView";
     }
 }
